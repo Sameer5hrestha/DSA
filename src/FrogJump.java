c3bf8b2e0b755eb10964ba0e9a425792a1079e78
sameer Shrestha <sameer.shresthaa@gmail.com>
2023-12-29 12:58:03 +0530

    FrogJump/Staircase added

--- src/FrogJump.java
+++ src/FrogJump.java
@@ -1,27 +1,60 @@
 import java.util.Arrays;
-import java.util.Scanner;
 public class FrogJump {
 
+
     static int solve(int index, int[] height, int[] dp) {
-        if (index == 0)
-            return 0;
-        if (dp[index] != -1)
-            return dp[index];
-        int oneJump = solve(index - 1, height, dp) + Math.abs(height[index] - height[index - 1]);
-        int twoJump = Integer.MAX_VALUE;
-        if (index > 1)
-            twoJump = solve(index - 2, height, dp) + Math.abs(height[index] - height[index - 2]);
-        return dp[index] = Math.min(oneJump, twoJump);
+
+        /* Memoization TC=O(N), SC=O(N) */
+//        if (index == 0)
+//            return 0;
+//        if (dp[index] != -1)
+//            return dp[index];
+//        int oneJump = solve(index - 1, height, dp) + Math.abs(height[index] - height[index - 1]);
+//        int twoJump = Integer.MAX_VALUE;
+//        if (index > 1)
+//            twoJump = solve(index - 2, height, dp) + Math.abs(height[index] - height[index - 2]);
+//        return dp[index] = Math.min(oneJump, twoJump);
+
+
+        /* Tabulation TC=O(N), SC=O(N) */
+//        dp[0]=0;
+//        for(index=1;index<height.length;index++){
+//            int jumpTwo=Integer.MAX_VALUE;
+//            int jumpOne=dp[index-1]+Math.abs(height[index]-height[index-1]);
+//            if(index>1)
+//                jumpTwo=dp[index-2]+ Math.abs(height[index]-height[index-2]);
+//            dp[index]=Math.min(jumpOne,jumpTwo);
+//
+//        }
+//        return dp[height.length-1];
+
+
+        /* tabulation space optimiztion TC=O(N),SC=O(1) */
+        int prev=0;
+        int prevTwo=0;
+
+        for(index=1;index<height.length;index++){
+            int jumpTwo=Integer.MAX_VALUE;
+            int jumpOne=prev+Math.abs(height[index]-height[index-1]);
+            if(index>1)
+                jumpTwo=prevTwo+ Math.abs(height[index]-height[index-2]);
+            int curr=Math.min(jumpOne,jumpTwo);
+            prevTwo=prev;
+            prev =curr;
+
+
+        }
+        return prev;
     }
 
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+
         int n;
-        n = sc.nextInt();
+        n = 6;
         int[] height = {30, 10, 60, 10, 60, 50};
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
-        System.out.println(solve(n - 1, height, dp));
+        System.out.println(solve(0, height, dp));
     }
 }
 
